6cccbf65113024be2a1dd317029d907f1e267507
sakshigautam09 <122722826+sakshigautam09@users.noreply.github.com>
2026-03-10 10:54:00 +0530

    [SakshiGautam]Add.added console for UC-14 (Temperature Measurement with Selective Arithmetic Support and IMeasurable Refactoring)

---
+++ QuantityMeasurementApp.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using QuantityMeasurementApp.Console;
+using QuantityMeasurementApp.Core.Interfaces;
+using QuantityMeasurementApp.Core.Services;
+
+namespace QuantityMeasurementApp.Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            ILengthService service = new LengthService();
+            IWeightService weightService = new WeightService();
+            IVolumeService volumeService = new VolumeService();
+            var temperatureService = new TemperatureService();
+
+            var menu = new Menu(service, weightService, volumeService, temperatureService);
+            menu.Show();
+        }
+    }
+}
